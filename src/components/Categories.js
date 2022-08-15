45cdc80d00a72229198c26671f0268f8f0165531
Htet Naing <htetnaing0814@gmail.com>
2022-08-16 00:11:40 +0630

    dispatch check status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,12 @@
-const Categories = () => <button type="button">Check Status</button>;
+import { checkStatus } from '../redux/categories/categories';
+import store from '../redux/configureStore';
+
+const Categories = () => {
+  const clickCheckStatus = () => {
+    store.dispatch(checkStatus());
+    console.log(store.getState().categories);
+  };
+  return (<button type="button" onClick={clickCheckStatus}>Check Status</button>);
+};
 
 export default Categories;
