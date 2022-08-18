81046edb9f706cdf26b1c33d1f80e08fe8e45b11
Htet Naing <htetnaing0814@gmail.com>
2022-08-18 14:40:02 +0630

    removed consoles

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,8 @@
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
-  const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(checkStatus());
@@ -11,7 +10,6 @@ const Categories = () => {
 
   const clickCheckStatus = () => {
     dispatch(checkStatus());
-    console.log(categories);
   };
   return (<button type="button" onClick={clickCheckStatus}>Check Status</button>);
 };
